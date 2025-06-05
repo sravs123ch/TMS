f48f34eff41444961e5e97cb1a4285117a4991e9
sravs123ch <ch.sravani6309@gmail.com>
2025-06-05 21:53:26 +0530

    commit files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//    assetsInclude: ['**/*.lottie'],
+//   server: {
+//     port: 7001,
+//   },
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-   assetsInclude: ['**/*.lottie'], 
+  assetsInclude: ["**/*.lottie"],
   server: {
-    port: 7001, 
+    port: 7001,
+    proxy: {
+      "/api": {
+        target: "http://82.180.147.10:7002",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
   },
-})
+});
